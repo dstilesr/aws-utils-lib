63244e2576172a9ada0a29141ce67581d1936855
dstilesr <d.stiles.r@hotmail.com>
2021-05-22 18:46:19 -0500

    Rename packages

--- python-lib/setup.py
+++ python-lib/setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ds_utils_lib",
-    version="0.0.2",
-    description="A data science utilities library",
+    name="aws_utils_lib",
+    version="0.0.3",
+    description="A utilities library to interact with aws",
     author="David Stiles Rosselli",
     install_requires=[
         "boto3",
         "fire<1.0.0",
-        "python-dotenv"
+        "python-dotenv",
+        "tensorflow>=2.0.0",
+        "numpy>1.0.0"
     ],
     packages=find_packages()
 )
