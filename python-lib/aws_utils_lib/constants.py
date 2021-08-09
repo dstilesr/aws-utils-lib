c78433aafb95bfcb286d5b6d53dbab312482b544
dstilesr <d.stiles.r@hotmail.com>
2021-08-09 17:18:32 -0500

    Added path to env file to constants module

--- python-lib/aws_utils_lib/constants.py
+++ python-lib/aws_utils_lib/constants.py
@@ -5,3 +5,6 @@ META_DIR: str = os.path.join(
     os.path.expanduser("~"),
     ".ds-utils-data"
 )
+
+"""Path to library's .env file."""
+ENV_PATH: str = os.path.join(META_DIR, ".env")
--- python-lib/aws_utils_lib/load_env.py
+++ python-lib/aws_utils_lib/load_env.py
@@ -1,10 +1,9 @@
-import os
 import dotenv
-from .constants import META_DIR
+from .constants import ENV_PATH
 
 
 def load_env_vars():
     """
     Loads the environment variables from META_DIR/.env.
     """
-    dotenv.load_dotenv(os.path.join(META_DIR, ".env"))
+    dotenv.load_dotenv(ENV_PATH)
