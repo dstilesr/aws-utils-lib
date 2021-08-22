8e665791cf62f865a61509fcc6d3e48a7db78941
dstilesr <d.stiles.r@hotmail.com>
2021-08-22 09:10:08 -0500

    Bump up lib version

--- python-lib/setup.py
+++ python-lib/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aws_utils_lib",
-    version="0.2.0",
+    version="0.3.0",
     description="A utilities library to interact with aws",
     author="David Stiles Rosselli",
     install_requires=[
